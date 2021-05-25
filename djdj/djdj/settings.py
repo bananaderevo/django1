198b433c1738642957fa198e799555777a9b399b
Аршинов Влад <vladikarshinov55@gmail.com>
2021-05-25 19:05:04 +0300

    all done

--- djdj/djdj/settings.py
+++ djdj/djdj/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = secret
+SECRET_KEY = (os.environ.get("SECRET_KEY", 'django-insecure-s1(%1pg#0)(1=!n=%=p+8l=rh2*%pg!e(t(im%@(21j-5qa$21'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
